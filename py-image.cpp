5df22a41bcbcebc2400716f0f6de0c9671ac496e
gruszczrob <gruszczrob@gmail.com>
2024-07-30 16:31:33 +0200

    Inicialization

---
+++ py-image.cpp
@@ -0,0 +1,12 @@
+﻿// py-image.cpp : Defines the entry point for the application.
+//
+
+#include "py-image.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
